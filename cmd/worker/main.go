90883efcbfeb6505968aaa84454616175ce7ca4d
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-10-31 02:57:12 +0100

    add support for temporal cloud
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/tls"
+	"encoding/base64"
 	"log/slog"
 	"os"
 
@@ -18,9 +20,17 @@ func main() {
 
 	logger.Info("starting worker", slog.String("version", version), slog.String("revision", version), slog.String("revisionDate", version))
 
+	var connectionOptions client.ConnectionOptions
+
+	if lookupEnv("TEMPORAL_TLS", "false") == "true" {
+		connectionOptions.TLS = loadTLS(logger)
+	}
+
 	temporalClient, err := client.Dial(client.Options{
-		HostPort: client.DefaultHostPort,
-		Logger:   log.NewStructuredLogger(logger.With(slog.String("subsystem", "temporal"))),
+		HostPort:          lookupEnv("TEMPORAL_ADDRESS", client.DefaultHostPort),
+		Namespace:         lookupEnv("TEMPORAL_NAMESPACE", "default"),
+		Logger:            log.NewStructuredLogger(logger.With(slog.String("subsystem", "temporal"))),
+		ConnectionOptions: connectionOptions,
 	})
 	if err != nil {
 		logger.Error("unable to create Temporal Client", slog.Any("error", err))
@@ -39,4 +49,64 @@ func main() {
 
 		os.Exit(1)
 	}
+}
+
+func lookupEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	return fallback
+}
+
+func loadTLS(logger *slog.Logger) *tls.Config {
+	certFile := lookupEnv("TEMPORAL_TLS_CERT_FILE", "")
+	keyFile := lookupEnv("TEMPORAL_TLS_KEY_FILE", "")
+
+	certContent := lookupEnv("TEMPORAL_TLS_CERT", "")
+	keyContent := lookupEnv("TEMPORAL_TLS_KEY", "")
+
+	if certFile != "" && keyFile != "" {
+		cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+		if err != nil {
+			logger.Error("unable to load TLS files", slog.Any("error", err), slog.String("cert", certFile), slog.String("key", keyFile))
+
+			os.Exit(1)
+		}
+
+		return &tls.Config{
+			Certificates: []tls.Certificate{cert},
+		}
+	} else if certContent != "" && keyContent != "" {
+		decodedCertContent, err := base64.StdEncoding.DecodeString(certContent)
+		if err != nil {
+			logger.Error("unable to decode certificate", slog.Any("error", err))
+
+			os.Exit(1)
+		}
+
+		decodedKeyContent, err := base64.StdEncoding.DecodeString(keyContent)
+		if err != nil {
+			logger.Error("unable to decode key", slog.Any("error", err))
+
+			os.Exit(1)
+		}
+
+		cert, err := tls.X509KeyPair(decodedCertContent, decodedKeyContent)
+		if err != nil {
+			logger.Error("unable to load TLS key pair", slog.Any("error", err))
+
+			os.Exit(1)
+		}
+
+		return &tls.Config{
+			Certificates: []tls.Certificate{cert},
+		}
+	}
+
+	logger.Error("TLS is enabled but no certificate is provided")
+
+	os.Exit(1)
+
+	return nil
 }
